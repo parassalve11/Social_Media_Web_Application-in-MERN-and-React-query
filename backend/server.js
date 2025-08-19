32bf4de98a43a80065cf640ae1cc153544105686
Paras Salve <160144558+parassalve1@users.noreply.github.com>
2025-08-19 20:11:43 +0530

    feat: Update CORS configuration and enhance axios instance for development mode

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(cookieParser());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 const PORT = process.env.PORT || 5000;
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,13 @@ export default defineConfig({
   plugins: [react(),
      tailwindcss(),
   ],
+  server:{
+    proxy:{
+      "/api":{
+        target:"http://localhost:3000"
+      }
+    },
+  },
  compilerOptions: {
     paths: {
       "@/*": ["./*"]
